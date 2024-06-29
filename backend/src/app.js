54065123846fd2082781f35e7d58244769c218e0
arnabw <arnabsahawrk@gmail.com>
2024-06-29 19:41:25 +0600

    added register api

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,4 +16,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//routes import
+import useRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", useRouter);
+
 export default app;
