95a55ef51519009869b19f8b58eb3dcbc04fd65a
arnabw <arnabsahawrk@gmail.com>
2024-07-27 03:56:43 +0600

    update: health check controller

--- backend/src/controllers/healthcheck.controller.js
+++ backend/src/controllers/healthcheck.controller.js
@@ -3,7 +3,7 @@ import apiResponse from "../utils/apiResponse.js";
 import asyncHandler from "../utils/asyncHandler";
 
 const healthCheck = asyncHandler(async (req, res) => {
-  //TODO: build a healthCheck response that simply returns the OK status as json with a message
+  //TODO: build a health check response that simply returns the OK status as json with a message
 });
 
 export { healthCheck };
