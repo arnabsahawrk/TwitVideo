fcfa8409fa5db6c5e96a0c7c2deea7ef5e45648e
arnabw <arnabsahawrk@gmail.com>
2024-07-27 04:20:46 +0600

    add: health check route

--- backend/src/controllers/healthcheck.controller.js
+++ backend/src/controllers/healthcheck.controller.js
@@ -1,6 +1,6 @@
 import apiError from "../utils/apiError.js";
 import apiResponse from "../utils/apiResponse.js";
-import asyncHandler from "../utils/asyncHandler";
+import asyncHandler from "../utils/asyncHandler.js";
 
 const healthCheck = asyncHandler(async (req, res) => {
   //TODO: build a health check response that simply returns the OK status as json with a message
