66eaa6978dfaf54c927dd212c5f81b095a51f22f
arnabw <arnabsahawrk@gmail.com>
2024-07-27 03:49:34 +0600

    update: comment controller

--- backend/src/controllers/comment.controller.js
+++ backend/src/controllers/comment.controller.js
@@ -1,3 +1,7 @@
+import mongoose from "mongoose";
+import Comment from "../models/comment.model.js";
+import apiError from "../utils/apiError.js";
+import apiResponse from "../utils/apiResponse.js";
 import asyncHandler from "../utils/asyncHandler";
 
 const getVideoComments = asyncHandler(async (req, res) => {
